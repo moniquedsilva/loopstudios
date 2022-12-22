3718ee2b272e8bceee3408233f08354dd1a780ed
Monique Silva <moniquesntsilva@gmail.com>
2022-12-22 00:01:52 -0300

    feat: add hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="home" class="h-screen">
+    <picture class="h-full w-full">
+      <img src="/assets/desktop/image-hero.jpg" alt="hero image" class="w-full h-full object-cover object-center">
+    </picture>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LoopsHero'
+}
+</script>
+
+<style>
+
+</style>
