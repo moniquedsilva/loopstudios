d3debca2e895375871c648312b268ed9be460406
Monique Silva <moniquesntsilva@gmail.com>
2022-12-22 00:00:55 -0300

    feat: add rules

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-white': 'hsl(0, 0%, 100%)',
+        'brand-black': 'hsl(0, 0%, 0%)',
+        'brand-dark-gray': 'hsl(0, 0%, 55%)',
+      },
+      backgroundColor: {
+        'brand-white': 'hsl(0, 0%, 100%)',
+        'brand-black': 'hsl(0, 0%, 0%)',
+        'brand-dark-gray': 'hsl(0, 0%, 55%)',
+      },
+      fontFamily: {
+        'alata': ['Alata', 'sans-serif'],
+        'josefin-sans': ['Josefin Sans', 'sans-serif'],
+      }
+    }
   },
   plugins: [],
 }
