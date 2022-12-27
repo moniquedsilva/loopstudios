e45627e7089fdf2bd108554c28499351035c7d6d
Monique Silva <moniquesntsilva@gmail.com>
2022-12-26 23:38:09 -0300

    feat: fixes and responsive navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,24 @@
 <template>
-  <header class="fixed w-full h-[105px] flex flex-wrap justify-between items-center px-6">
-    <a href="#home">
-      <img src="/assets/logo.svg" alt="Logo" class="w-44 h-max md:w-48">
-    </a>
-    <button @click="toogleMenu" class="cursor-pointer transition-all rotate-0 duration-700 ease-out" :class="{'rotate-180': menuOpen}">
-      <img :src="menuOpen ? closeButton : hamburgerButton" alt="Menu">
-    </button>
-    <nav class="w-full">
-      <ul class="bg-brand-black h-0 opacity-0 transition-all duration-300" :class="{'h-44 opacity-100': menuOpen}">
+  <header class="fixed w-full max-w-[1114px] flex p-6 transition-transition duration-300" :class="menuOpen ? 'h-screen bg-brand-black inset-0 items-start flex-col' : 'h-[105px] justify-between items-center'">
+    <div class="w-full flex justify-between md:w-max">
+      <a href="#home">
+        <img src="/assets/logo.svg" alt="Logo" class="w-44 h-max md:w-48">
+      </a>
+      <button @click="toogleMenu" class="cursor-pointer transition-all rotate-0 duration-700 ease-out md:hidden" :class="{'rotate-180': menuOpen}">
+        <img :src="menuOpen ? closeButton : hamburgerButton" alt="Menu">
+      </button>
+    </div>
+    <nav v-if="menuOpen" class="w-full h-full flex flex-col justify-center">
+      <ul class="pl-8 flex flex-col gap-y-4 absolute w-full overflow-hidden bg-brand-black left-0 right-0 font-josefin-sans uppercase">
         <li v-for="link in navLinks" :key="link.name">
-          <a :href="link.href" class="text-white text-lg font-bold hover:text-[#FBBF24] transition-all duration-300 ease-out">{{ link.name }}</a>
+          <a :href="link.href" class="nav__links text-white text-2xl font-bold hover:text-brand-dark-gray transition-all ease-out">{{ link.name }}</a>
+        </li>
+      </ul>
+    </nav>
+    <nav v-else class="hidden md:block">
+      <ul class="space-x-8">
+        <li v-for="link in navLinks" :key="link.name" class="inline-block">
+          <a :href="link.href" class="nav__links text-white text-[15px] font-semibold ease-out transition-all">{{ link.name }}</a>
         </li>
       </ul>
     </nav>
@@ -55,3 +64,19 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.nav__links:after {
+  content: "";
+  display: block;
+  width: 0;
+  height: 2px;
+  margin: 0 auto;
+  margin-top: 8px;
+  background: none repeat scroll 0 0 rgb(255, 255, 255);
+  transition: all ease-in-out 0.3s;
+}
+.nav__links:hover:after {
+  width: 24px;
+}
+</style>
