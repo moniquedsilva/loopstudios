441f746eeb64ab97e1cf36d50ea6df00118373d2
Monique Silva <moniquesntsilva@gmail.com>
2023-01-01 21:37:51 -0300

    feat: add creations

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center bg-brand-white">
     <Header />
-    <main class="bg-gray-500">
+    <main>
       <Hero />
       <About />
+      <Creations />
       <Footer />
     </main>
   </div>
@@ -13,6 +14,7 @@
 import Header from './components/Header.vue'
 import Hero from './components/Hero.vue'
 import About from './components/About.vue'
+import Creations from './components/Creations.vue'
 import Footer from './components/Footer.vue'
 
 export default {
@@ -21,6 +23,7 @@ export default {
     Header,
     Hero,
     About,
+    Creations,
     Footer
   }
 }
