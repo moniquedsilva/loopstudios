65ba7f43aa6f5261925c7805348760c372552860
Monique Silva <moniquesntsilva@gmail.com>
2023-01-01 21:39:26 -0300

    feat: toggle desk and mobile img

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,11 @@
 <template>
-  <div id="home" class="h-screen">
+  <div id="home" class="h-screen mb-[184px]">
     <div class="mx-auto w-full max-w-[1138px] flex items-center justify-center lg:justify-start absolute inset-0 p-6">
-      <h1 class="max-w-[327px] md:max-w-[652px] h-max p-6 pb-4 font-josefin-sans leading-none uppercase tracking-wide text-[40px] md:text-7xl text-white border-[3px] border-white">Immersive experiences that deliver</h1>
+      <h1 class="max-w-[327px] md:max-w-[652px] h-max p-6 pb-4 font-josefin-sans leading-none uppercase tracking-wide text-[40px] md:text-7xl text-white border-[3px] border-white z-[2]">Immersive experiences that deliver</h1>
     </div>
-    <picture class="h-full w-full">
-      <img src="/assets/desktop/image-hero.jpg" alt="hero image" class="w-full h-full object-cover object-center">
+    <picture class="hero h-full w-full">
+      <source srcset="/assets/desktop/image-hero.jpg" media="(min-width: 750px)">
+      <img src="/assets/mobile/image-hero.jpg" alt="hero image" class="w-full h-full object-cover">
     </picture>
   </div>
 </template>
@@ -14,3 +15,18 @@ export default {
   name: 'LoopsHero'
 }
 </script>
+
+<style scoped>
+@media(min-width: 768px) {
+  .hero::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+    background: rgba(0, 0, 0, 0.3);
+  }
+}
+</style>
